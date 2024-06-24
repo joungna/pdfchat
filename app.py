fa7ca6981d10a616460d42e577565114c1b7729d
AI Knowledge.Redistributor and UrbanAcupuncturist <44631748+joungna@users.noreply.github.com>
2024-06-24 12:21:57 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+__import__('pysqlite3')
 import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 import tempfile
 import os
@@ -6,9 +9,10 @@ import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings 
 from langchain_community.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
+
 
 # 제목
 st.title("ChatPDF")
